6ed5edf17e558de16967175df6fc1535fcd887e0
Robert Tate <btate712@gmail.com>
2019-03-13 20:45:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,14 +4,14 @@ class Anagram
   attr_accessor :word
 
   def initialize(input_word)
-    word = input_word
+    @word = input_word
   end
 
   def match(array_of_possible_anagrams)
     return_value = nil
     array_of_possible_anagrams.each do |word_to_test|
       binding.pry
-      if word_to_test.chars.sort == word.chars.sort
+      if word_to_test.chars.sort == @word.chars.sort
         return_value = word_to_test
       end
     end
