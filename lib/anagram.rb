adacb40e4eaccb78eaf6c2bbb418722cf6a1291a
Robert Tate <btate712@gmail.com>
2019-03-13 20:42:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(input_word)
+    word = input_word
+  end
+  
+  def match(array_of_possible_anagrams)
+    return_value = nil
+    array_of_possible_anagrams.each do |word_to_test|
+      if word_to_test.to_a.sort == word.to_a.sort
+        return_value = word_to_test
+      end
+    end
+    return_value
+  end
+end
+
