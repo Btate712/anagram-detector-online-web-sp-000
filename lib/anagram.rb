7f978a0a68982da1603dbaf42bc25b67b7e995f6
Robert Tate <btate712@gmail.com>
2019-03-13 20:43:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   def match(array_of_possible_anagrams)
     return_value = nil
     array_of_possible_anagrams.each do |word_to_test|
-      if word_to_test.char.sort == word.char.sort
+      if word_to_test.chars.sort == word.chars.sort
         return_value = word_to_test
       end
     end
